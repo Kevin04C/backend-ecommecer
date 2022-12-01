97146b416944de5ad95bee4585123e7605bd4bd4
Kevin04C <kevincespedes2004@gmai.com>
2022-11-30 21:19:13 -0500

    add route uploadPhoto

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,6 +4,8 @@ import bycrypt from "bcrypt";
 import { encryptPassword } from "../helpers/encryptPassword.js";
 import { generateJWT } from "../helpers/jwt.js";
 import { revalidateToken } from "../middleware/revalidateToken.js";
+import { validatePhoto } from "../middleware/validatePhoto.js";
+import { removePhoto, uploadPhoto } from "../utilities/cloudinary.js";
 
 const router = Router();
 
@@ -18,7 +20,6 @@ router.post("/api/auth/crearUsuario", (req, res) => {
     contacto2,
     email,
     password,
-    foto,
     idRol,
   } = req.body;
 
@@ -32,14 +33,15 @@ router.post("/api/auth/crearUsuario", (req, res) => {
     contacto2,
     email,
     password,
-    foto,
     idRol,
   };
 
   data.password = encryptPassword(password);
-  let sqlSearchEmail = `SELECT * FROM USUARIO WHERE email = '${email}'`;
-  let sqlSearchDni = `SELECT * FROM USUARIO WHERE dni = '${dni}'`;
-  let sql = "INSERT INTO USUARIO SET ?";
+  const sqlSearchEmail = `SELECT * FROM USUARIO WHERE email = '${email}'`;
+  const sqlSearchDni = `SELECT * FROM USUARIO WHERE dni = '${dni}'`;
+  const sql = "INSERT INTO USUARIO SET ?";
+  const sqlInsertPhoto = "INSERT INTO FOTO_USUARIO (idUsuario) VALUES (?)";
+  const sqlGetPhoto = "SELECT secure_url from FOTO_USUARIO WHERE idUsuario = ?";
 
   connection.query(sqlSearchEmail, function (error, result) {
     if (error) {
@@ -57,14 +59,47 @@ router.post("/api/auth/crearUsuario", (req, res) => {
             } else {
               connection.query(sql, data, async function (error, result) {
                 if (error) {
-                  throw error;
+                  return res.status(500).json({
+                    ok: false,
+                    msg: "Algo salió mal",
+                  });
                 } else {
                   delete data.password;
                   const token = generateJWT(data);
                   data.token = token;
 
-                  data.idUsuario = result.insertId;
-                  res.send(data);
+                  const idUsuario = result.insertId;
+                  data.idUsuario = idUsuario;
+
+                  connection.query(
+                    sqlInsertPhoto,
+                    [idUsuario],
+                    (error, result) => {
+                      if (error) {
+                        console.log(error);
+                        return res.status(500).json({
+                          ok: false,
+                          msg: "Algo salió mal",
+                        });
+                      }
+                      connection.query(
+                        sqlGetPhoto,
+                        [idUsuario],
+                        (error, result) => {
+                          if (error) {
+                            return res.status(500).json({
+                              ok: false,
+                              msg: "Algo salió mal",
+                            });
+                          }
+                          res.status(200).json({
+                            ...data,
+                            secure_url: result[0].secure_url,
+                          });
+                        }
+                      );
+                    }
+                  );
                 }
               });
             }
@@ -76,11 +111,13 @@ router.post("/api/auth/crearUsuario", (req, res) => {
 });
 //VALIDA USUARIO
 router.post("/api/auth/login", (req, res) => {
-  let email = req.body.email;
-  let password = req.body.password;
+  const email = req.body.email;
+  const password = req.body.password;
 
-  let sqlForUser = `SELECT * FROM USUARIO WHERE email='${email}'`;
-  connection.query(sqlForUser, async (error, result) => {
+  const queryGetUser = `SELECT * FROM USUARIO WHERE email=?`;
+  const queryGetPhoto = `SELECT secure_url FROM FOTO_USUARIO WHERE idUsuario=?`;
+
+  connection.query(queryGetUser, email, async (error, result) => {
     if (error) {
       throw error;
     } else {
@@ -90,9 +127,28 @@ router.post("/api/auth/login", (req, res) => {
         if (passwordCompare) {
           delete result[0].password;
 
-          const token = await generateJWT(result[0]);
+          const token = generateJWT(result[0]);
           result[0].token = token;
-          res.send(result[0]);
+          const user = result[0];
+          connection.query(
+            queryGetPhoto,
+            result[0].idUsuario,
+            (error, result) => {
+              if (error) {
+                console.log(error);
+                return res.status(500).json({
+                  ok: false,
+                  msg: "Algó salio mal",
+                });
+              }
+              const { secure_url } = result[0];
+              res.status(200).json({
+                ok: true,
+                ...user,
+                secure_url,
+              });
+            }
+          );
         } else {
           res.send({ ok: false, message: "Contraseña incorrecta" });
         }
@@ -107,11 +163,24 @@ router.post("/api/auth/login", (req, res) => {
 router.post("/api/renew", revalidateToken, async (req, res) => {
   const payloadToken = req.body;
   const token = generateJWT(payloadToken);
+  const query = "SELECT secure_url FROM FOTO_USUARIO WHERE idUsuario = ?";
 
-  res.send({
-    ok: true,
-    ...payloadToken,
-    token,
+  connection.query(query, payloadToken.idUsuario, (error, result) => {
+    if (error) {
+      console.log(error);
+      return res.status(500).json({
+        ok: false,
+        msg: "Algo salió mal",
+      });
+    }
+    const { secure_url } = result[0];
+
+    res.status(200).json({
+      ok: true,
+      ...payloadToken,
+      secure_url,
+      token,
+    });
   });
 });
 
@@ -141,8 +210,6 @@ router.put("/api/auth/actualizarUsuario/:id", (req, res) => {
     direccion,
     contacto1,
     contacto2,
-    fechaNacimiento,
-    foto,
   } = req.body;
 
   let data = {
@@ -152,17 +219,16 @@ router.put("/api/auth/actualizarUsuario/:id", (req, res) => {
     direccion,
     contacto1,
     contacto2,
-    fechaNacimiento,
-    foto,
     idUsuario,
   };
-  let sql = `UPDATE USUARIO SET nombre=?,apellidoPaterno=?,apellidoMaterno=?,direccion=?,contacto1=?,contacto2=?,fechaNacimiento=?,foto=? WHERE idUsuario=${idUsuario}`;
+  let sql = `UPDATE USUARIO SET nombre=?,apellidoPaterno=?,apellidoMaterno=?,direccion=?,contacto1=?,contacto2=? WHERE idUsuario=?`;
   const arrayData = Array.from(Object.values(data));
 
   connection.query(sql, arrayData, function (error, results) {
     if (error) {
+      console.log(error);
       return res.status(500).json({
-        ok: faslse,
+        ok: false,
         msg: "Algo no salió bien",
       });
     }
@@ -172,6 +238,76 @@ router.put("/api/auth/actualizarUsuario/:id", (req, res) => {
       user: { ...data },
     });
   });
+});
+
+router.put("/api/auth/uploadPhoto/:id", validatePhoto, async (req, res) => {
+  const { id } = req.params;
+  const photo = req.files.photo;
+  const query = "SELECT * FROM FOTO_USUARIO WHERE idUsuario = ?";
+  const queryUpdate =
+    "UPDATE FOTO_USUARIO SET secure_url = ?, public_id = ? WHERE idUsuario = ?";
+  let public_id;
+
+  try {
+    connection.query(query, id, async (error, result) => {
+      if (error) {
+        return res.status(500).json({
+          ok: false,
+          msg: "Algo salió mal",
+        });
+      }
+      public_id = result[0]["public_id"];
+
+      if (!public_id) {
+        const { secure_url, public_id } = await uploadPhoto(photo.tempFilePath);
+        connection.query(
+          queryUpdate,
+          [secure_url, public_id, id],
+          (error, result) => {
+            if (error) {
+              return res.status(500).json({
+                ok: false,
+                msg: "Algo salió mal",
+              });
+            }
+            res.status(200).json({
+              ok: true,
+              secure_url,
+            });
+          }
+        );
+      } else {
+        await removePhoto(public_id);
+
+        const { secure_url, public_id: public_id_new } = await uploadPhoto(
+          photo.tempFilePath
+        );
+        connection.query(
+          queryUpdate,
+          [secure_url, public_id_new, id],
+          (error, result) => {
+            if (error) {
+              return res.status(500).json({
+                ok: false,
+                msg: "Algo salió mal",
+              });
+            }
+
+            res.status(200).json({
+              ok: true,
+              secure_url,
+            });
+          }
+        );
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Ups, algo salió mal",
+    });
+  }
 });
 
 export default router;
